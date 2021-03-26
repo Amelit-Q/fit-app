f865f892bf8fa796a0e802645ac00aee0c2786a7
Muradov Tarik <muradovt111@gmail.com>
2021-03-26 22:08:43 +0300

    added logic for User class and Gender class describe

--- FitnessAppBL/Model/Gender.cs
+++ FitnessAppBL/Model/Gender.cs
@@ -1,7 +1,36 @@
-﻿namespace FitnessAppBL.Model
+﻿using System;
+
+namespace FitnessAppBL.Model
 {
+    /// <summary>
+    /// Пол.
+    /// </summary>
     public class Gender
     {
+        /// <summary>
+        /// Название пола.
+        /// </summary>
+        public string Name { get; }
         
+        /// <summary>
+        /// Конструктор. Создать новый пол
+        /// </summary>
+        /// <param name="name">Название пола</param>
+        /// <exception cref="ArgumentNullException"></exception>
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name), "Is Empty please enter your name");
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
--- FitnessAppBL/Model/User.cs
+++ FitnessAppBL/Model/User.cs
@@ -1,7 +1,77 @@
-﻿namespace FitnessAppBL.Model
+﻿using System;
+
+namespace FitnessAppBL.Model
 {
+    /// <summary>
+    /// Class User
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// User's gender (string)
+        /// </summary>
+        public Gender Gender { get; }
+
+        /// <summary>
+        /// User's birthday 
+        /// </summary>
+        public DateTime BirthDay { get; }
+
+        /// <summary>
+        /// Weight of the user
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        ///  User's height
+        /// </summary>
+        public double Height { get; set; }
+
         
+        /// <summary>Constructor for new user</summary>
+        public User(string name, Gender gender, DateTime birthDay, double weight, double height)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name), "Name can't be empty or null");
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException(nameof(gender), "gender field can't be null");
+            }
+
+            if (birthDay < DateTime.Parse("01.01.1940") || birthDay >= DateTime.Now)
+            {
+                throw new ArgumentException("Birthday incorrect", nameof(birthDay));
+            }
+
+            if (weight <= 20)
+            {
+                throw new ArgumentException("Weight can't be under 20", nameof(weight));
+            }
+
+            if (height <= 100)
+            {
+                throw new ArgumentException("Height can't be less than 100, or equals it", nameof(height));
+            }
+
+
+            Name = name;
+            Gender = gender;
+            BirthDay = birthDay;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
