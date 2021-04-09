03214242a5791fe53641c21da67b802e541270e9
p.tretyakov <p.tretyakov@simplex-software.ru>
2021-04-09 15:50:44 +0300

    Added New Controller

---
+++ FitnessAppBL/Controller/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FitnessAppBL.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+
+        public UserController(User user)
+        {
+            User = user ?? throw new ArgumentNullException(nameof(user), "user can't be null or white space");
+        }
+
+        // public bool Save() 1.17
+        // {
+        //     
+        // }
+    }
+}
